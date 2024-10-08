972da485659e4078d197fb2a491c90587bbfcea1
zaryaab-chatta <zaryaab.chatta@torontomu.ca>
2024-10-08 14:56:41 -0400

    test

--- Lab5_Excercises/PartA.js
+++ Lab5_Excercises/PartA.js
@@ -6,3 +6,13 @@
 function myFunction(){
     alert("Hello!")
 }
+
+function mOver(some_tag) {
+    some_tag.innerHTML = "Thank You";
+    some_tag.style.color = "blue";
+}
+
+function mOut(some_tag) {
+    some_tag.innerHTML = "Mouse Over Me";
+    some_tag.style.color = "orange";
+}
