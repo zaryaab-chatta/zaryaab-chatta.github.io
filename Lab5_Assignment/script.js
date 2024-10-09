c80fdc1e0d7945f7e6ddff14cb0cce5518a97b0e
zaryaab-chatta <zaryaab.chatta@torontomu.ca>
2024-10-09 13:04:03 -0400

    Assignment

---
+++ Lab5_Assignment/script.js
@@ -0,0 +1,11 @@
+// Function to prompt the user for their name and display it
+function promptForName() {
+    let userName = prompt("Please enter your name:");
+
+    // If a name is entered, display it in the HTML element
+    if (userName) {
+        document.getElementById("displayName").innerText = "Hello, " + userName + "!";
+    } else {
+        document.getElementById("displayName").innerText = "You didn't enter a name.";
+    }
+}
