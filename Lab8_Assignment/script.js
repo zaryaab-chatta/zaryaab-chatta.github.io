77a686b51f6c01b70f9ca7fb936d88eb7bcad889
zaryaab-chatta <zaryaab.chatta@torontomu.ca>
2024-11-12 18:46:53 -0500

    basic

---
+++ Lab8_Assignment/script.js
@@ -0,0 +1,14 @@
+// Function to increase text size
+function increaseTextSize() {
+    document.body.style.fontSize = '1.2em';
+}
+
+// Function to toggle color scheme for colorblind mode
+function toggleColorScheme() {
+    document.body.classList.toggle('dark-mode');
+}
+
+// Function to display alt-text for images
+function showAltText(altText) {
+    alert(altText);
+}
