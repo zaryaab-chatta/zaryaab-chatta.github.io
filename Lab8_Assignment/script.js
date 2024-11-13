4a17fd2e032d26c23036db2650a52fc29f52292a
zaryaab-chatta <zaryaab.chatta@torontomu.ca>
2024-11-12 19:57:55 -0500

    Update script.js

--- Lab8_Assignment/script.js
+++ Lab8_Assignment/script.js
@@ -5,7 +5,7 @@ function toggleTextSize() {
 
 // Function to toggle color scheme for colorblind mode
 function toggleColorScheme() {
-    document.body.classList.toggle('colorblind-mode');
+    document.body.classList.toggle('dark-mode');
 }
 
 // Function to display alt-text for images
